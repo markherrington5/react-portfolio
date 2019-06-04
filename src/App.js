19fb550275f45a173f54309bc0300394244234cb
Mark <markherrington5@gmail.com>
2019-06-03 21:47:40 -0400

    top margin still wont go away - no changes actually made

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter, Route, Link } from 'react-router-dom';
-import './App.css';
 import homepage from './pages/homepage';
 import photography from './pages/photography';
 
