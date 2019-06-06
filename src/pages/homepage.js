76400e904c33b8dabf664bf861790298a7c107fc
Mark <markherrington5@gmail.com>
2019-06-05 20:38:51 -0500

    renamed LgName.js temporarily

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import LgName from "../components/LgName.js";
+import LgName from "./LgName";
 import "../styles/homepage.css";
 import portrait from "../images/portrait.jpg";
 import githubLogo from "../images/githubLogo.svg";
