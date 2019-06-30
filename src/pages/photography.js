4cd4024af3c263dce1b487f5e292bba309389644
Mark <markherrington5@gmail.com>
2019-06-30 18:44:24 -0500

    added fourth photo entry, renamed photo filenames

--- src/pages/photography.js
+++ src/pages/photography.js
@@ -5,9 +5,10 @@ import Col from "react-bootstrap/Col";
 import HomeBtn from "../components/homeBtn.js";
 import PhotoHeader from "../components/photoHeader.js";
 import "../styles/photography.css";
-import photo from "../images/photography/201610_Rome_01 AV-1, Vista 200.jpg";
-import photo2 from "../images/photography/201708_Crosstown_01 124G, HP5.jpg";
-import photo3 from "../images/photography/201802_Beale_19 35RC, T-Max 400.jpg";
+import photo from "../images/photography/photo1.jpg";
+import photo2 from "../images/photography/photo2.jpg";
+import photo3 from "../images/photography/photo3.jpg";
+import photo4 from "../images/photography/photo4.jpg";
 
 const photography = () => (
   <div>
@@ -17,22 +18,44 @@ const photography = () => (
         <Col>
           <PhotoHeader />
           <div className="photoblock landscape">
-            <img src={photo} alt="201610_Rome_01 AV-1, Vista 200" className='photo'/>
-            <p className='imgDesc'>Rome, Italy</p>
-            <p className='imgDesc'>Canon AV-1 / Agfa Vista 200</p>
-            <p className='imgDesc'>October 2016</p>
+            <img
+              src={photo}
+              alt="201610_Rome_01 AV-1, Vista 200"
+              className="photo"
+            />
+            <p className="imgDesc">Rome, Italy</p>
+            <p className="imgDesc">Canon AV-1 / Agfa Vista 200</p>
+            <p className="imgDesc">October 2016</p>
           </div>
           <div className="photoblock square">
-            <img src={photo2} alt="201708_Crosstown_01 124G, HP5" className="photo" />
-            <p className='imgDesc'>Memphis, TN, USA</p>
-            <p className='imgDesc'>Yashica Mat-124G / Ilford HP5</p>
-            <p className='imgDesc'>August 2017</p>
+            <img
+              src={photo2}
+              alt="201708_Crosstown_01 124G, HP5"
+              className="photo"
+            />
+            <p className="imgDesc">Memphis, TN, USA</p>
+            <p className="imgDesc">Yashica Mat-124G / Ilford HP5</p>
+            <p className="imgDesc">August 2017</p>
           </div>
           <div className="photoblock portrait">
-            <img src={photo3} alt="201802_Beale_19 35RC, T-Max 400" className="photo" />
-            <p className='imgDesc'>Memphis, TN, USA</p>
-            <p className='imgDesc'>Olympus 35RC / Kodak T-Max 400</p>
-            <p className='imgDesc'>February 2018</p>
+            <img
+              src={photo3}
+              alt="201802_Beale_19 35RC, T-Max 400"
+              className="photo"
+            />
+            <p className="imgDesc">Memphis, TN, USA</p>
+            <p className="imgDesc">Olympus 35RC / Kodak T-Max 400</p>
+            <p className="imgDesc">February 2018</p>
+          </div>
+          <div className="photoblock landscape">
+            <img
+              src={photo4}
+              alt="201803_StLouisZoo_01 M645, Portra 160.jpg"
+              className="photo"
+            />
+            <p className="imgDesc">St. Louis, MO, USA</p>
+            <p className="imgDesc">Mamiya M645 Super / Kodak Portra 160</p>
+            <p className="imgDesc">March 2018</p>
           </div>
         </Col>
       </Row>
