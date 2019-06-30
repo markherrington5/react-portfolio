004f67d304df73b92eee03fa92858abc21158fb2
Mark <markherrington5@gmail.com>
2019-06-30 18:34:50 -0500

    added 3 entries to photo page

---
+++ src/components/photoHeader.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../styles/photoHeader.css";
+
+const PhotoHeader = () => (
+    <h1 id="photoTitle">
+        <div class="animatePhotoTitle shortDelay">Photography</div>
+    </h1>
+);
+
+export default PhotoHeader;
+
--- src/pages/photography.js
+++ src/pages/photography.js
@@ -2,9 +2,12 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import HomeBtn from '../components/homeBtn';
+import HomeBtn from "../components/homeBtn.js";
+import PhotoHeader from "../components/photoHeader.js";
 import "../styles/photography.css";
 import photo from "../images/photography/201610_Rome_01 AV-1, Vista 200.jpg";
+import photo2 from "../images/photography/201708_Crosstown_01 124G, HP5.jpg";
+import photo3 from "../images/photography/201802_Beale_19 35RC, T-Max 400.jpg";
 
 const photography = () => (
   <div>
@@ -12,8 +15,25 @@ const photography = () => (
     <Container id="s1">
       <Row>
         <Col>
-          <h2>Photography</h2>
-          <img src={photo} alt="photo1" id="photo1" />
+          <PhotoHeader />
+          <div className="photoblock landscape">
+            <img src={photo} alt="photo1" className='photo'/>
+            <p className='imgDesc'>Rome, Italy</p>
+            <p className='imgDesc'>Canon AV-1 / Agfa Vista 200</p>
+            <p className='imgDesc'>October 2016</p>
+          </div>
+          <div className="photoblock square">
+            <img src={photo2} alt="photo2" className="photo" />
+            <p className='imgDesc'>Memphis, TN, USA</p>
+            <p className='imgDesc'>Yashica Mat-124G / Ilford HP5</p>
+            <p className='imgDesc'>September 2017</p>
+          </div>
+          <div className="photoblock portrait">
+            <img src={photo3} alt="photo3" className="photo" />
+            <p className='imgDesc'>Memphis, TN, USA</p>
+            <p className='imgDesc'>Olympus 35RC / Kodak T-Max 400</p>
+            <p className='imgDesc'>February 2018</p>
+          </div>
         </Col>
       </Row>
     </Container>
