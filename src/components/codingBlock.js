b3070b75f4d6f80fe40e57674f23c8946233bf36
Mark <markherrington5@gmail.com>
2019-07-16 15:53:31 -0400

    ran npm install and npm audit fix

--- src/components/codingBlock.js
+++ src/components/codingBlock.js
@@ -16,36 +16,3 @@ const CodingBlock = props => (
   );
   
   export default CodingBlock;
-
-//   <div class="portfolioDiv">
-//             <h5>Ingles Markets Redesign</h5>
-//             <a href="https://ingles-2.herokuapp.com/" target="_blank">
-//               <img class="portfolioImg" src="./images/inglesthumb.jpg" />
-//             </a>
-
-//             <p>
-            //   For our third and final project, my group decided to create a
-            //   proof-of-concept overhaul of an existing company's site. We chose
-            //   Ingles, a modest regional supermarket chain with an outdated web
-            //   presence. "Ingles 2.0" presents users with an attractive interface
-            //   and exclusive discounts and rewards for loyal customers who choose
-            //   to share their information with the site. The project only
-            //   concerns itself with the most important pages on the site, but is
-            //   organized for ease of expansion. It makes use of backend
-            //   authentication, React.js, and MongoDB to store produts, selected
-            //   specials, credentials, and users' feedback messages to the
-            //   company.
-//             </p>
-//             <a
-//               href="https://ingles-2.herokuapp.com/"
-//               target="_blank"
-//               class="portfolioBtn"
-//               >Heroku Deployment</a
-//             >
-//             <a
-//               href="https://github.com/stefeifert/Ingles-2.0/"
-//               target="_blank"
-//               class="portfolioBtn"
-//               >Github</a
-//             >
-//           </div>
