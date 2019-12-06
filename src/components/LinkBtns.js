9b6faa5587a8dabd20b6b8fcca2b7fb7cbf24659
Mark H <markherrington5@gmail.com>
2019-12-06 15:55:03 -0500

    removed WIP coding projects page

--- src/components/LinkBtns.js
+++ src/components/LinkBtns.js
@@ -3,7 +3,7 @@ import '../styles/LinkBtns.css';
 
 const LinkBtns = () => (
     <div>
-        <a className='linkBtn' href='./coding'>Coding Projects</a>
+        {/* <a className='linkBtn' href='./coding'>Coding Projects</a> */}
         <a className='linkBtn' href='./photography'>Photography</a>
     </div>
 );
